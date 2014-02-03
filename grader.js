bdf7abc79a2a823f6cf48978f2ba5776d84d4cba
Ubuntu <ubuntu@ip-172-31-35-197.us-west-2.compute.internal>
2014-02-03 08:22:09 +0000

    final grader

--- grader.js
+++ grader.js
@@ -26,8 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
-var URL_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -57,19 +57,6 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-
-var checkHtmlString = function(htmlString, checksfile) {
-    $ = cheerio.load(htmlString);
-	console.log( "$ =\n" + $);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
-};
-
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -78,26 +65,40 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <html_file>', 'Path to index.html')
+        //.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        //.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        //.option('-u, --url <iurl_site>', 'Path to index.html', URL_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json')
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <iurl_site>', 'Path to index.html')
         .parse(process.argv);
-//console.log("Checking " + program.url )
-   var  testString ; 
- rest.get(program.url).on('complete',function(testString){ 
-console.log("HTML output\n" + testString)
+var checkJson;
+var outJson;
+console.log( "program.file = " +program.file + "\n" );
+console.log( "program.checks = " +program.checks + "\n" );
+console.log( "program.url = " +program.url + "\n" );
+if (program.file)
+{
+    checkJson = checkHtmlFile(program.file, program.checks);
+    outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
 }
-);
-//console.log(testString)
-if ( program.url) {
-console.log("in proj");
-    var checkJson = checkHtmlString(testString, program.checks);
+else if( program.url)
+{
+	console.log("coming to the right place \n");
+       rest.get(program.url).on('complete', function(data) {
+        $ = cheerio.load(data);
+        var checks = loadChecks(program.checks).sort();
+        var out = {};
+        for(var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
+        outJson = JSON.stringify(out, null, 4);
+        console.log(outJson);
+	});
 
 }
-    //var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
-
